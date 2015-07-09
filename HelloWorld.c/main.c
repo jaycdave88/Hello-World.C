accae46646c5c2f0fd0116d4a4cba4fdcc86cc42
Jay Dave <jaycdave@gmail.com>
2015-07-09 15:58:48 -0500

    adding readme file to github - first commit

---
+++ HelloWorld.c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld.c
+//
+//  Created by DEV MODE on 7/9/15.
+//  Copyright (c) 2015 DEV MODE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+###Hello World 
+* Working on understanding Objective-C better so diving into C 
+
+> Jay C. Davé
